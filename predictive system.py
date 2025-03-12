76fb9f83ba8021954dcaf964465035dd1c9af29a
kemboivictor <91088086+kemboivictor@users.noreply.github.com>
2025-03-12 13:51:58 +0300

    Add files via upload

---
+++ predictive system.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pickle 
+
+# loading the saved model, read binary
+loaded_model = pickle.load(open('E:/notes/y4/project/deploying/trained_model.sav', 'rb'))
+
+input_data = (5,109,75,26,0,36,0.546,60)
+
+# changing the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the array as we are predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction = loaded_model.predict(input_data_reshaped)
+print(prediction)
+
+if (prediction[0] == 0):
+  print('The person is not diabetic')
+else:
+  print('The person is diabetic')
